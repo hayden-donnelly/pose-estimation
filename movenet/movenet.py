59774984753e7247e93ea8b643e1c0d584972025
hayden-donnelly <donnellyhd@outlook.com>
2023-03-29 22:43:47 -0400

    Printing out keypoints in readable form.

--- movenet/movenet.py
+++ movenet/movenet.py
@@ -1,5 +1,20 @@
 import tensorflow as tf
 import tensorflow_hub as hub
+import numpy as np
+
+ordered_keypoint_labels = [
+    'nose', 'left_eye', 'right_eye', 'left_ear', 'right_ear', 'left_shoulder',
+    'right_shoulder', 'left_elbow', 'right_elbow', 'left_wrist', 'right_wrist',
+    'left hip', 'right hip', 'left knee', 'right knee', 'left ankle', 'right ankle'
+]
+
+def render_keypoints(image_width, image_height, keypoints, confidence_threshold):
+    scaled_keypoints = np.multiply(keypoints, [image_width, image_height, 1])
+
+    for i in range(scaled_keypoints.shape[0]):
+        x, y, score = scaled_keypoints[i]
+        is_confident = score > confidence_threshold
+        print(ordered_keypoint_labels[i], x, y, score, is_confident)
 
 print(tf.config.list_physical_devices('GPU'))
 
@@ -18,7 +33,11 @@ movenet = model.signatures['serving_default']
 # Run model inference.
 outputs = movenet(image)
 # Output is a [1, 6, 56] tensor.
-keypoints = outputs['output_0']
+output = outputs['output_0'].numpy()
+print("output shape:", output.shape)
 
+# Throw away bounding box, then reshape.
+keypoints = output[0, :6, :17*3].reshape(1, 6, 17, 3)
 print("keypoints shape:", keypoints.shape)
-print(keypoints)
+
+render_keypoints(256, 256, keypoints[0, 0], 0.5)
