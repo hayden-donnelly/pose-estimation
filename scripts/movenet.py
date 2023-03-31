65f2766ebf5d85ff6d01357146f6ba033ce30d08
hayden-donnelly <donnellyhd@outlook.com>
2023-03-31 16:18:33 -0400

    Added pose estimation for videos.

--- scripts/movenet.py
+++ scripts/movenet.py
@@ -30,55 +30,91 @@ edges = [
     (14, 16),
 ]
 
-print(tf.config.list_physical_devices('GPU'))
+confidence_threshold = 0.1
 
-# Load the input image.
-image = cv2.imread('../data/input/basketball_guy.jpg')
-image_height, image_width, _ = image.shape
-tf_image = tf.image.resize_with_pad(tf.expand_dims(image, axis=0), 384, 640)
-tf_image = tf.cast(tf_image, dtype=tf.int32)
+def draw_connections(image, keypoints):
+    for i in range(len(edges)):
+        index1, index2 = edges[i]
+        x1, y1, score1 = keypoints[index1]
+        x2, y2, score2 = keypoints[index2]
 
-# Download the model from TF Hub.
-model = hub.load("https://tfhub.dev/google/movenet/multipose/lightning/1")
-movenet = model.signatures['serving_default']
+        if(score1 > confidence_threshold and score2 > confidence_threshold):
+            cv2.line(image, (int(y1), int(x1)), (int(y2), int(x2)), (0, 0, 255), 4)
 
-# Run model inference.
-outputs = movenet(tf_image)
-output = outputs['output_0'].numpy()
-print("output shape:", output.shape)
 
-# Throw away bounding box, then reshape.
-keypoints = output[0, :6, :17*3].reshape(1, 6, 17, 3)
-print("keypoints shape:", keypoints.shape)
+def draw_keypoints(image, keypoints):
+    for i in range(keypoints.shape[0]):
+        x, y, score = keypoints[i]
+        if(score > confidence_threshold):
+            cv2.circle(image, (int(y), int(x)), 5, (0, 255, 0), -1)
 
-# Throw away extra keypoints.
-keypoints = keypoints[0, 0]
-print(keypoints.shape)
 
-# Scale keypoints to original image dimensions.
-scaled_keypoints = np.multiply(keypoints, [image_height, image_width, 1])
+def cv_to_tf(image):
+    tf_image = tf.image.resize_with_pad(tf.expand_dims(image, axis=0), 384, 640)
+    tf_image = tf.cast(tf_image, dtype=tf.int32)   
+    return tf_image
 
-confidence_threshold = 0.3
 
-# Print out keypoints in easy to read format.
-for i in range(scaled_keypoints.shape[0]):
-    x, y, score = scaled_keypoints[i]
-    is_confident = score > confidence_threshold
-    print(ordered_keypoint_labels[i], x, y, score, is_confident)
+def video_pose_estimation(input_data_path, model):
+    # Load the video.
+    video = cv2.VideoCapture(input_data_path)
 
-# Draw connections as lines.
-for i in range(len(edges)):
-    index1, index2 = edges[i]
-    x1, y1, score1 = scaled_keypoints[index1]
-    x2, y2, score2 = scaled_keypoints[index2]
+    # Check if the video was loaded successfully.
+    if not video.isOpened():
+        print("Error loading video file")
 
-    if(score1 > confidence_threshold and score2 > confidence_threshold):
-        cv2.line(image, (int(y1), int(x1)), (int(y2), int(x2)), (0, 0, 255), 4)
+    # Get the width and height of the video.
+    width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-# Draw keypoints as circles.
-for i in range(scaled_keypoints.shape[0]):
-    x, y, score = scaled_keypoints[i]
-    if(score > confidence_threshold):
-        cv2.circle(image, (int(y), int(x)), 5, (0, 255, 0), -1)
+    # Define the codec and create VideoWriter object.
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+    out = cv2.VideoWriter('../data/output/movenet_video_output.mp4', fourcc, 25.0, (width, height))
 
-cv2.imwrite("../data/output/movenet_output.jpg", image)
+    # Loop through the video frame by frame.
+    while video.isOpened():
+        # Read a frame from the video.
+        ret, frame = video.read()
+
+        # Check if the frame was read successfully.
+        if not ret:
+            break
+
+        # Run model inference.
+        outputs = model(cv_to_tf(frame))
+        output = outputs['output_0'].numpy()
+
+        # Throw away bounding box, then reshape.
+        keypoints = output[0, :6, :17*3].reshape(1, 6, 17, 3)
+
+        # Throw away extra keypoints.
+        keypoints = keypoints[0, 0]
+
+        # Scale keypoints to original image dimensions.
+        scaled_keypoints = np.multiply(keypoints, [height, width, 1])
+
+        # Draw connections as lines.
+        draw_connections(frame, scaled_keypoints)
+
+        # Draw keypoints as circles.
+        draw_keypoints(frame, scaled_keypoints)
+
+        # Write the modified frame to the output video.
+        out.write(frame)
+
+
+if __name__ == "__main__":
+    print(tf.config.list_physical_devices('GPU'))
+
+    # Download the model from TF Hub.
+    model = hub.load("https://tfhub.dev/google/movenet/multipose/lightning/1")
+    movenet = model.signatures['serving_default']
+
+    video_pose_estimation("../data/input/dance_video.mp4", movenet)
+    #video = cv2.VideoCapture('../data/input/dance_video_short.mp4')
+
+    # Check if the video was loaded successfully.
+    #if not video.isOpened():
+    #    print("Error loading video file")
+
+    
